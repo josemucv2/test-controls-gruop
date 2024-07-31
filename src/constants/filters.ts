cb065fad4da900751c32403eee9dae93686ea218
josemucv2 <martinezcaceresjosem@mail.com>
2024-07-31 10:12:01 -0400

    fix changes input for select un add character

---
+++ src/constants/filters.ts
@@ -0,0 +1,35 @@
+export const FILTERS_STATUS = [
+    {
+        _id: 1,
+        name: 'alive',
+    },
+
+    {
+        _id: 2,
+        name: 'dead',
+    },
+    {
+        _id: 3,
+        name: 'unknown',
+    },
+];
+
+export const FILTERS_GENDER = [
+    {
+        _id: 1,
+        name: 'Female',
+    },
+
+    {
+        _id: 2,
+        name: 'Male',
+    },
+    {
+        _id: 3,
+        name: 'Genderless',
+    },
+    {
+        _id: 4,
+        name: 'unknown',
+    },
+];
